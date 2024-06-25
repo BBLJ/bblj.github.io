db2c08aff9cff33bdf4d993223c2f4ddef45bc48
boychen <msit130foodma@gmail.com>
2024-06-25 12:14:54 +0800

    send

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ window.addEventListener("DOMContentLoaded", main);
 
 let isInLIFF = false;
 const txtResult = document.getElementById("txtResult");
+const txtErrMsg = document.getElementById("txtErrMsg");
 
 function main(){
     // Using a Promise object
@@ -27,5 +28,23 @@ function showUserProfile(){
         }
     } catch (ex) {
         txtResult.value = ex;
+    }
+};
+
+function sendMessage(){
+    txtErrMsg.value = "";
+    try {
+        liff.sendMessages([
+            {
+                type: "text",
+                text: "Hello, World!",
+            },
+        ]).then(() => {
+            
+        }).catch((err) => {
+            txtErrMsg.value = err;
+        });
+    } catch (ex){
+        txtErrMsg.value = ex;
     }
 };
